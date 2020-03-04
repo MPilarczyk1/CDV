5e52b0334c0cb1b977520896eba1caa53c9c7146
Mikołaj Pilarczyk <mpilarczyk@edu.cdv.pl>
2020-03-04 15:28:13 +0100

    done

--- Strukturalne/1_podstawy.py
+++ Strukturalne/1_podstawy.py
@@ -1,1 +1,36 @@
 print("CDV")
+print(10)
+'''
+komentarz
+blokowy
+'''
+#potęgowanie
+potega=2 **10
+print(potega)
+
+#pobieranie danych z klawiatury
+name=input()
+#+oznacza konkatenacje
+print("Twoje imie to: "+name)
+nazwisko=input()
+print("Twoje imie to "+name+", nazwisko "+nazwisko)
+'''
+użytkownik podaje z klawiatury swój wiek z klawiatury,
+wyświetl dane w formacie : Twoj wiek:... lat:...
+'''
+#end="" to zakończenie ciągiem pustym
+print("Podaj swój wiek", end="")
+age=input()
+print(type(age))
+print("Twój wiek ",age," lat")
+age1=20
+print(type(age1))
+surname="Kowalski"
+firstLetter=surname[0]
+print(firstLetter)
+
+lenght = len(surname)
+print(lenght)
+
+lastLetter=surname[len(surname)-1]
+print(lastLetter)
